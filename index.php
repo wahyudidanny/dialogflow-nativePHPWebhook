299a5703da35c5bdb0a443a09c78c35c2711b4ce
Mr Not So Smart <wahyudidanny23@gmail.com>
2018-07-08 21:59:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,40 @@
-<?php
+<?php 
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
-	$json = json_decode{$requestBody};
-	
-	$text = $json->result->paramteres->text;
-	
-	switch($text){
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
 		case 'hi':
-			$speech = "Hi, Nice To Meet You"
+			$speech = "Good Bye, see you later then";
 			break;
-			
+
 		case 'bye':
-			$speech = "Good Bye, see you later then"
+			$speech = "Good Bye, see you later then";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here";
 			break;
 		
-		case 'anything':
-			$speech = "Yes, you can type anything here"
-			break;	
-			
 		default:
 			$speech = "Sorry i dont catch your meaning. Please ask something else";
 			break;
 	}
-	
+
 	$response = new \stdClass();
-	$response->speech = "";
-	$response->displayText = "";
-	$response->source ="webhook";
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
 	echo json_encode($response);
-	
-}	
-	else
-	{
-		echo"Method not allowed"
-		
-		
-	}
+}
+else
+{
+	echo "Method not allowed";
+}
 
 ?>
