71230c7fd35cd2bdb234ad025455eaf4810d35df
Danny Wahyudi <wahyudidanny23@gmail.com>
2018-07-10 23:00:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if($method == 'POST'){
 	}
 
 	$response = new \stdClass();
-	$response->speech = $speech;
+	$response->fulfillmentText = $speech;
 	$response->displayText = $speech;
 	$response->source = "webhook";
 	echo json_encode($response);
